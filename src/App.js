40720bfed5ba2bbddd34ff22a367831c3c7de85b
rkiang <ryanlkiang@gmail.com>
2018-03-08 17:14:38 -0600

    added login page and installed router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import logo from './images/PCitTogetherLogo.png';
 import './App.css';
+
+// pages
+import Login from './pages/Login/Login';
 
 class App extends Component {
   render() {
@@ -10,7 +14,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">PC It Together</h1>
         </header>
-   
+        <Router>
+          <div className="Links">
+            <ul>
+              <li><Link to='/'>Login</Link></li>
+            </ul>
+            <Switch>
+              <Route exact path='/' component={Login} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
