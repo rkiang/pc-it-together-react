ea9552ca0718640b4337277c43b7c0ba6215c823
rkiang <ryanlkiang@gmail.com>
2018-03-09 14:09:50 -0600

    added password input with form and page does not refresh

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,40 +1,54 @@
 import React, { Component } from 'react';
 
-// const Login = () => (
-//     <div>
-//         <h2>Login</h2>
-//     </div>
-// );
-
 class Login extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             username: '',
+            password: '',
         }
-        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
         this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
     }
 
-    handleInputChange(event) {
+    // Handler for username input
+    handleUsernameChange(event) {
         this.setState({
-          username: event.target.value,
+            username: event.target.value
         });
-      }
+    }
 
-    handleLoginSubmit() {
+    // Handler for password input
+    handlePasswordChange(event) {
+        this.setState({
+            password: event.target.value
+        });
+    }
+
+    handleLoginSubmit(e) {
         console.log('button was clicked');
         console.log('username is: ' + this.state.username);
+        console.log('password is: ' + this.state.password);
+        e.preventDefault(); // prevents page refresh
         this.setState({
-            
+            username: '',
+            password: '',
         })
     }
 
     render() {
         return (
             <div>
-                <input type="text" value={this.state.username} onChange={this.handleInputChange} />
-                <button onClick={this.handleLoginSubmit}>Login</button>
+                <form onSubmit={this.handleLoginSubmit}>
+                    <label>Username:
+                    <input type="text" value={this.state.username} onChange={this.handleUsernameChange} />
+                    </label>
+                    <label>Password:
+                    <input type="password" value={this.state.password} onChange={this.handlePasswordChange} />
+                    </label>
+                    <button>Login</button>
+                </form>
             </div>
         );
     }
