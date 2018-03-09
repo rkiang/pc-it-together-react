fabc782556b67addf54b42ef598877e845b2d088
rkiang <ryanlkiang@gmail.com>
2018-03-08 18:01:40 -0600

    added input field for username

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Login = () => (
-    <div>
-        <h2>Login</h2>
-    </div>
-);
+// const Login = () => (
+//     <div>
+//         <h2>Login</h2>
+//     </div>
+// );
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        this.setState({
+          username: event.target.value,
+        });
+      }
+
+    handleLoginSubmit() {
+        console.log('button was clicked');
+        console.log('username is: ' + this.state.username);
+        this.setState({
+            
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" value={this.state.username} onChange={this.handleInputChange} />
+                <button onClick={this.handleLoginSubmit}>Login</button>
+            </div>
+        );
+    }
+}
 
 export default Login;
