c57c69039c3da993a20666d5007da9696733a01f
rkiang <ryanlkiang@gmail.com>
2018-03-22 11:29:47 -0500

    added fields for registering new user

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Register = () => (
-    <div>
-        <h2>Register</h2>
-    </div>
-);
+// const Register = () => (
+class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+        }
+        // this.addUsers = this.addUsers.bind(this);
+        // this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        // this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+    //     
+    //     
+    // </div>
+    render() {
+        return (
+            <div>
+                <h2>Register</h2>
+                <form onSubmit={this.addUsers}>
+                    <label>Username:
+                    <input type="text" value={this.state.username} onChange={this.handleUsernameChange} />
+                    </label>
+                    <label>Password:
+                    <input type="password" value={this.state.password} onChange={this.handlePasswordChange} />
+                    </label>
+                    <button>Register</button>
+                </form>
+            </div>
+        )
+    }
+};
 
 export default Register;
